076c486e78aad92b518c47b87a9dbd64a7d52c7c
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2022-12-27 22:39:14 +0300

    fix

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,67 +5,67 @@ const NotFoundError404 = require('../errors/notFoundError');
 const ForbiddenError403 = require('../errors/forbiddenError');
 
 module.exports.createMovie = (req, res, next) => {
-	const {
-		country,
-		director,
-		duration,
-		year,
-		description,
-		image,
-		trailerLink,
-		thumbnail,
-		movieId,
-		nameRU,
-		nameEN,
-	} = req.body;
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
 
-	const owner = req.user._id;
+  const owner = req.user._id;
 
-	Movie.create({
-		country,
-		director,
-		duration,
-		year,
-		description,
-		image,
-		trailerLink,
-		thumbnail,
-		owner,
-		movieId,
-		nameRU,
-		nameEN,
-	})
-		.then((movie) => res.send(movie))
-		.catch((err) => {
-			if (err.name === 'ValidationError') {
-				next(new BadRequestError400('Некорректные данные одного из полей'));
-			} else {
-				next(err);
-			}
-		});
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    owner,
+    movieId,
+    nameRU,
+    nameEN,
+  })
+    .then((movie) => res.send(movie))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError400('Некорректные данные одного из полей'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 module.exports.getMovies = (req, res, next) => {
-	Movie.find({})
-		.then((movies) => res.send(movies))
-		.catch(next);
+  Movie.find({})
+    .then((movies) => res.send(movies))
+    .catch(next);
 };
 
 module.exports.deleteMovieById = (req, res, next) => {
-	const { movieId } = req.params;
-	Movie.findById(movieId)
-		.orFail(() => new NotFoundError404('Фильм по указанному _id не найден'))
-		.then((movie) => {
-			if (!movie.owner.equals(req.user._id)) {
-				return next(new ForbiddenError403('Пытаетесь удалить чужой фильм'));
-			}
-			return movie.remove().then(() => res.status(200).send({ message: 'Фильм удален' }));
-		})
-		.catch((err) => {
-			if (err.name === 'CastError') {
-				next(new BadRequestError400('Фильма с таким _id не существует'));
-			} else {
-				next(err);
-			}
-		});
+  const { movieId } = req.params;
+  Movie.findById(movieId)
+    .orFail(() => new NotFoundError404('Фильм по указанному _id не найден'))
+    .then((movie) => {
+      if (!movie.owner.equals(req.user._id)) {
+        return next(new ForbiddenError403('Пытаетесь удалить чужой фильм'));
+      }
+      return movie.remove().then(() => res.status(200).send({ message: 'Фильм удален' }));
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new BadRequestError400('Фильма с таким _id не существует'));
+      } else {
+        next(err);
+      }
+    });
 };
