1eb660bd4b6a3b13f8ccdb1abb44b88e19fc2ccd
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-02-21 14:28:08 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,23 +7,26 @@ const AuthError401 = require('../errors/authError');
 const { needAuthUser } = require('../errors/errorsConstantsList');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+	const token = req.cookies.jwt;
 
-  console.log(token);
+	console.log(`TOKEN ${token}`);
+	console.log(`JWT_SECRET ${JWT_SECRET}`);
+	console.log(`NODE_ENV ${NODE_ENV}`);
 
-  if (!token) {
-    return next(new AuthError401(`${needAuthUser} 1`));
-  }
 
-  let payload;
+	if (!token) {
+		return next(new AuthError401(`${needAuthUser} 1`));
+	}
 
-  try {
-    payload = jsonwebtoken.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-  } catch (err) {
-    return next(new AuthError401(`${needAuthUser} 2`));
-  }
+	let payload;
 
-  req.user = payload;
+	try {
+		payload = jsonwebtoken.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+	} catch (err) {
+		return next(new AuthError401(`${needAuthUser} 2`));
+	}
 
-  return next();
+	req.user = payload;
+
+	return next();
 };
