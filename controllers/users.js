8c4bc2d3d5c6ababfd88e31ffcd139be95c18e61
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-02-28 17:47:39 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -33,7 +33,7 @@ module.exports.loginUser = (req, res, next) => {
 					sameSite: 'none',
 					httpOnly: true,
 					secure: true,
-					domain: 'snv-project-movies.ru',
+					domain: ['snv-project-movies.ru', 'api-snv-project-movies.ru'],
 				})
 				.send({ token, message: `Выполнен вход в аккаунт ${user.email}` });
 		})
