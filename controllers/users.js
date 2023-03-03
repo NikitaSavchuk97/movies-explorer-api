6828d6e950f6e5b3974027aca661c3945151c9e4
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-03-03 18:31:51 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -29,7 +29,7 @@ module.exports.loginUser = (req, res, next) => {
 			);
 			res
 				.cookie('jwt', token, {
-					maxAge: 3600000 * 24 * 7,
+					maxAge: 1800000,
 					sameSite: 'none',
 					secure: true,
 				})
