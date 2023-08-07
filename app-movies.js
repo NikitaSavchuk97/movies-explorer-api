62eedac30f62e5d94868db145d917376a0d61d4e
NikitaSavchuk97 <99026028+NikitaSavchuk97@users.noreply.github.com>
2023-08-07 11:59:31 +0300

    frn-link

--- app-movies.js
+++ app-movies.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const cors = require('cors');
+const XLSX = require('xlsx'); // лишнее
 const helmet = require('helmet');
 const express = require('express');
 const mongoose = require('mongoose');
@@ -12,22 +13,41 @@ const serverError = require('./middlewares/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const {
-  PORT,
-  NODE_ENV,
-  MOVIES_EXPLORER_DB,
-  DEFAULT_DB,
+	PORT,
+	NODE_ENV,
+	MOVIES_EXPLORER_DB,
+	DEFAULT_DB,
 } = process.env;
 
 const app = express();
 
 app.use(cors({
-  origin: allowedCors.allowedUrls,
-  methods: allowedCors.allowedMethods,
-  credentials: true,
+	origin: allowedCors.allowedUrls,
+	methods: allowedCors.allowedMethods,
+	credentials: true,
 }));
 
 mongoose.set('strictQuery', false);
 mongoose.connect(NODE_ENV === 'production' ? MOVIES_EXPLORER_DB : DEFAULT_DB);
+
+
+
+// ------------------------------------------------------ Лишнее
+app.get('/frn-users', (req, res) => {
+	const filePath = './user-list/user-list.xlsx';
+	const workbook = XLSX.readFile(filePath);
+	// Получение имени первого листа
+	const firstSheetName = workbook.SheetNames[0];
+	// Получение первого листа
+	const worksheet = workbook.Sheets[firstSheetName];
+	// Преобразование данных из листа в JSON
+	const data = XLSX.utils.sheet_to_json(worksheet);
+	console.log(data)
+	res.send(data)
+});
+//---------------------------------------------------------
+
+
 
 app.use(helmet());
 app.use(cookieParser());
@@ -38,10 +58,12 @@ app.use(requestLogger);
 app.use(limiter);
 app.use(require('./routes/router'));
 
+
+
 app.use(errorLogger);
 app.use(errors());
 app.use(serverError);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+	console.log(`App listening on port ${PORT}`);
 });
